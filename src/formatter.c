35c33c1f8cbf29c50a5e32ee5e2a129c1a84e71d
rohit <rohit.880207@gmail.com>
2025-07-22 20:20:19 +0530

    added size_t instead of hardcoded size

--- src/formatter.c
+++ src/formatter.c
@@ -4,14 +4,15 @@
 
 char* format_response(char *body){
     char* buffer = malloc(sizeof(char)*4096);
-    snprintf(buffer, sizeof(buffer)*4096, 
+    size_t body_len = strlen(body);
+    snprintf(buffer,4096, 
               "HTTP/1.1 200 OK\r\n"
               "Content-Type: text/html;"
               "charset=UTF-8\r\n"
-              "Content-Length: 4096\r\n"
-              "Connection: close\r\n"
+              "Content-Length: %zu\r\n"
+              "Connection:close\r\n"
               "\r\n"
-             "%s"
-             ,body);
+             "%s\n"
+             ,body_len,body);
     return buffer;
 }
