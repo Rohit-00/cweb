46d9bce72afbbe5d06cfa592a4147700e9ea1326
rohit <rohit.880207@gmail.com>
2025-07-26 16:46:05 +0530

    added get_messages function

--- src/database.c
+++ src/database.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include "parser.h"
+#include <stdlib.h>
+
+body_t* get_messages(){
+
+  FILE *file = fopen("../database/database.db","rb");
+
+  fseek(file, 0, SEEK_END);
+  long file_size = ftell(file);
+  rewind(file);
+
+  int count = file_size/sizeof(body_t);
+  body_t *messages = malloc(sizeof(body_t)*count);
+
+  fread(messages,sizeof(body_t),count,file);
+  return messages;
+}
 
 int add_message(body_t *body){
 
   FILE *file = fopen("../database/database.db","a+b");
   if(!file){
     perror("file");
+    return -1;
   }
 
   fwrite(body,sizeof(body_t),1,file);
   fclose(file); 
+  return 0;
 }
+
+
