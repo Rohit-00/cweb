45d8874fb84706056855ff61555203f4d9358356
rohit <rohit.880207@gmail.com>
2025-07-28 14:21:51 +0530

    created header files and common structures

--- include/formatter.h
+++ include/formatter.h
@@ -2,4 +2,7 @@
 #define FORMATTER_H
 
 char* format_response(char *body);
+char* format_json_response(char *json_body);
+char* format_error(char *body);
+
 #endif 
--- include/parser.h
+++ include/parser.h
@@ -3,9 +3,16 @@
 
 typedef struct {
   char method[10];
-  char path[100];
+  char path[256];
   char version[20];
 }request_t;
 
+typedef struct {
+  char username[82];
+  char message[256];
+} body_t;
+
 request_t parse(char *req_string);
+body_t parse_body(char *req_string);
+
 #endif
